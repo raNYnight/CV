8f1ebebe2003e256e920ce2421e132c5aca0f4a9
raNYnight <rany4ka@gmail.com>
2023-01-18 15:38:34 +0300

    feat:add hamburger menu, optimized css

--- index.js
+++ index.js
@@ -2,29 +2,39 @@ const carousel = document.querySelector(".carousel");
 const sliderLeft = document.querySelector(".slider_left");
 const sliderRight = document.querySelector(".slider_right");
 const pageNumberNode = document.querySelector("#page-number");
-let page = 1;
 let moveLength = 500;
+let page = 1;
+
+window.addEventListener("resize", () => {
+  if (document.body.offsetWidth < 691) {
+    moveLength = 300;
+    carousel.style.left = "0px";
+  }
+  if (document.body.offsetWidth > 691) {
+    moveLength = 500;
+    carousel.style.left = "0px";
+  }
+});
 
 sliderRight.addEventListener("click", moveRight);
-sliderRight.classList.add("active");
+sliderRight.classList.add("active-btn");
 carousel.addEventListener("transitionend", (event) => {
   sliderLeft.addEventListener("click", moveLeft);
   sliderRight.addEventListener("click", moveRight);
   if (page > 1) {
-    sliderLeft.classList.add("active");
+    sliderLeft.classList.add("active-btn");
   }
   if (page === 1) {
     sliderLeft.removeEventListener("click", moveLeft);
-    sliderLeft.classList.remove("active");
+    sliderLeft.classList.remove("active-btn");
   }
   if (page === 9) {
     sliderRight.removeEventListener("click", moveRight);
-    sliderRight.classList.remove("active");
+    sliderRight.classList.remove("active-btn");
   }
   if (page < 9) {
-    sliderRight.classList.add("active");
+    sliderRight.classList.add("active-btn");
   }
-  console.log(event);
 });
 {
 }
@@ -46,4 +56,27 @@ function moveRight() {
   carousel.style.left = `${left}px`;
   sliderLeft.removeEventListener("click", moveLeft);
   sliderRight.removeEventListener("click", moveRight);
+}
+
+const hamburger = document.querySelector(".hamburger"),
+  menu = document.querySelector(".header-nav"),
+  menuLinks = document.querySelectorAll(".nav-link"),
+  line = document.querySelectorAll(".line");
+
+hamburger.addEventListener("click", burgerFunc);
+menu.addEventListener("click", (e) => {
+  let target = e.target;
+  if (target.classList.contains("nav-link")) {
+    burgerFunc();
+  }
+  if (!target.matches("nav-item") && menu.classList.contains("open")) {
+    burgerFunc();
+  }
+});
+function burgerFunc() {
+  menu.classList.toggle("open");
+  hamburger.classList.toggle("active");
+  line.forEach((item) => {
+    item.classList.toggle("line-active");
+  });
 }
